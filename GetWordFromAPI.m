3e911a40f71f9a9db9465f6d754655ffb6b20eac
Suthikshnan Karthik <karthiksuthikshnan@gmail.com>
2021-05-13 10:12:30 -0400

    Removed API Key for future purposes

--- GetWordFromAPI.m
+++ GetWordFromAPI.m
@@ -6,7 +6,7 @@
 
 function Word = GetWordFromAPI(Word)
 api = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/';
-Key = '?key=78a49613-2600-4e65-8649-e9ea965d4719';
+Key = ''; % Requires a Dictionary API Key, place here in the ' '.
 Api = strcat(api,Word);
 Api = strcat(Api,Key);
 WordRead = webread(Api);
